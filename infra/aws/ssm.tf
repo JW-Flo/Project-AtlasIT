8d5f7efcf96b52edd00bca1dfbddb6b2c4c712f2
JW-Flo <joseph.whittle@gmail.com>
2026-04-09 08:48:32 -0500

    fix: SSM parameters use overwrite=true to handle pre-existing params

--- infra/aws/ssm.tf
+++ infra/aws/ssm.tf
@@ -2,54 +2,63 @@
 
 resource "aws_ssm_parameter" "console_bucket" {
   name  = "/atlasit/${var.env}/console-bucket"
-  type  = "String"
+  type      = "String"
+  overwrite = true
   value = aws_s3_bucket.console.bucket
 }
 
 resource "aws_ssm_parameter" "cloudfront_distribution_id" {
   name  = "/atlasit/${var.env}/cloudfront-distribution-id"
-  type  = "String"
+  type      = "String"
+  overwrite = true
   value = aws_cloudfront_distribution.main.id
 }
 
 resource "aws_ssm_parameter" "api_gateway_url" {
   name  = "/atlasit/${var.env}/api-gateway-url"
-  type  = "String"
+  type      = "String"
+  overwrite = true
   value = aws_apigatewayv2_api.main.api_endpoint
 }
 
 resource "aws_ssm_parameter" "aurora_endpoint" {
   name  = "/atlasit/${var.env}/aurora-endpoint"
-  type  = "String"
+  type      = "String"
+  overwrite = true
   value = aws_rds_cluster.main.endpoint
 }
 
 resource "aws_ssm_parameter" "evidence_bucket" {
   name  = "/atlasit/${var.env}/evidence-bucket"
-  type  = "String"
+  type      = "String"
+  overwrite = true
   value = aws_s3_bucket.evidence.bucket
 }
 
 resource "aws_ssm_parameter" "sqs_step_tasks_url" {
   name  = "/atlasit/${var.env}/sqs-step-tasks-url"
-  type  = "String"
+  type      = "String"
+  overwrite = true
   value = aws_sqs_queue.step_tasks.url
 }
 
 resource "aws_ssm_parameter" "event_bus_name" {
   name  = "/atlasit/${var.env}/event-bus-name"
-  type  = "String"
+  type      = "String"
+  overwrite = true
   value = aws_cloudwatch_event_bus.atlasit.name
 }
 
 resource "aws_ssm_parameter" "jml_workflow_arn" {
   name  = "/atlasit/${var.env}/jml-workflow-arn"
-  type  = "String"
+  type      = "String"
+  overwrite = true
   value = aws_sfn_state_machine.jml_workflow.arn
 }
 
 resource "aws_ssm_parameter" "automation_rule_arn" {
   name  = "/atlasit/${var.env}/automation-rule-arn"
-  type  = "String"
+  type      = "String"
+  overwrite = true
   value = aws_sfn_state_machine.automation_rule.arn
 }
